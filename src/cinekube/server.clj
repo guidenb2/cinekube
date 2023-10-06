07f0c42740d7e2adaa17c30bc5c8983af5c60449
Ben Guiden <ben.guiden2@mail.dcu.ie>
2023-10-06 23:54:04 +0100

    Added basic communication to movie tracker service

---
+++ src/cinekube/server.clj
@@ -0,0 +1,28 @@
+(ns cinekube.server
+  (:require [cinekube.configuration :as config]
+            [cinekube.service-movie-tracker :as movie-tracker]
+            [com.appsflyer.donkey.core :refer [create-donkey create-server]]
+            [com.appsflyer.donkey.server :refer [start]]
+            [com.appsflyer.donkey.result :refer [on-success]]
+            [reitit.ring :as ring]))
+
+
+(defn handler [_]
+  {:status 200, :body (:body (movie-tracker/currently-reading))})
+
+
+(def app
+  (ring/ring-handler
+    (ring/router
+      [["/tracker/watched" {:get handler}]])))
+
+
+(defn init-server []
+  (let [config (config/read-config)]
+    (->
+      (create-donkey)
+      (create-server {:port (:port config)
+                      :routes [{:handler app
+                                :handler-mode :blocking}]})
+      start
+      (on-success (fn [_] (println "Server started listening on port" (:port config)))))))
